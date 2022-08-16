181bde31196b2cced4c3a54200f16dd8ae6592fa
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2022-08-16 10:06:27 +0200

    initial commit

---
+++ src/app/post-item/post-item.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit , Input , Output , EventEmitter} from '@angular/core';
+import { Post } from '../models/post';
+
+@Component({
+  selector: 'app-post-item',
+  templateUrl: './post-item.component.html',
+  styleUrls: ['./post-item.component.css']
+})
+export class PostItemComponent implements OnInit {
+  @Input() post: Post;
+  @Output() hidePost : EventEmitter<Post> = new EventEmitter;
+
+  constructor() {
+    this.post = {
+    id: 1,
+    title: '',
+    body: '',
+    votes: 1
+   }
+  }
+
+  ngOnInit(): void {
+  }
+
+  upvote(post : Post) : void {
+    post.votes +=1 ;
+  }
+
+  downvote(post: Post): void{
+    post.votes -=1 ;
+  }
+
+  hide(post: Post): void{
+    this.hidePost.emit(post);
+  }
+
+
+
+}
